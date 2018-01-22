fc4c602b4842655be9b11018f5cc2f34ded2ec86
Huong Dang <huongd97@gmail.com>
2018-01-22 15:46:58 -0500

    Update

--- API/deleteContact.php
+++ API/deleteContact.php
@@ -1,7 +1,19 @@
 
 <?php
+
   if (isset($_GET['id']))
     echo "success";
   else
     echo "fail whale :(";
+
+    /*
+    $sql = "DELETE FROM (tablename) WHERE (what is passed in on Rachael's side)";
+
+    if ($conn->query($sql) == TRUE)
+      echo "Record deleted successfully"; // Need to return this in Json
+    else
+      echo "Error deleting record: ".$conn->error; // Need to return this in Json
+
+    $conn->close();
+    */
 ?>
--- API/viewContact.php
+++ API/viewContact.php
@@ -1,7 +1,10 @@
 
 <?php
+
   if (isset($_GET['id']))
     echo "success";
   else
     echo "fail whale :(";
+
+  
 ?>
