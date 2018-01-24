ed7e990f2a6f7a4f85df2917b7a4709667a8e6a0
Huong Dang <huongd97@gmail.com>
2018-01-24 14:52:36 -0500

    AddContact should work
    We still need to figure out userID into the database

--- API/addContact.php
+++ API/addContact.php
@@ -1,7 +1,47 @@
 <?php
-  if (isset($_POST['name']) and isset($_POST['phone']) and isset($_POST['email']))
-  	// Hey Huong, please return data in this format! Thank you! -Rachael
-    echo '{"id":"'.rand(0000,9999).'","name":"'.$_POST['name'].'","phone":"'.$_POST['phone'].'","email":"'.$_POST['email'].'"}';
-  else
+
+  // Verify that the user has entered a first, last, phone, and email
+  if (!(isset($_POST['nameF']) && isset($_POST['nameL']) && isset($_POST['phone']) && isset($_POST['email'])))
     echo "fail whale :(";
+
+  // Establishing database connection
+  $conn = new mysqli(servername, username, password, databasename);
+
+  // By this point, do I have to verify the correct
+  // User table that I should be looking at?
+  if ($conn->connect_error)
+  {
+    echo "fail whale :(";
+  }
+  else {
+    $sql = "INSERT INTO Contact(nameF, nameL, HomePh, Email) VALUES(";
+    $sql = $sql."'".mysqli_real_escape_string($_POST['nameF'])."', ";
+    $sql = $sql."'".mysqli_real_escape_string($_POST['nameL'])."', ";
+    $sql = $sql."'".mysqli_real_escape_string($_POST['phone'])."', ";
+    $sql = $sql."'".mysqli_real_escape_string($_POST['email'])."')";
+
+    if ($result = $conn->query($sql) != TRUE)
+    {
+      echo "fail whale :(";
+    }
+    else
+    {
+      echo '{"id":"'.rand(0000,9999).'","nameF":"'.$_POST['nameF'].'","nameL":"'.$_POST['nameF'].'","phone":"'.$_POST['phone'].'","email":"'.$_POST['email'].'"}';
+    }
+
+    $conn->close();
+  }
+  // Since there's only 'name' when we have two field names, I will split
+
+  // 'people' is the name of our table
+  // corresponding parameters for 'people' are the names of the columns
+  // corresponding parameters for VALUES are the corresponding insert stuff
+  // For adding contacts
+  // $strSQL = "INSERT INTO people(FirstName, LastName, Phone, Email) VALUES('fname', 'lname', 'phone', 'email')";
+  // mysql_query($strSQL) or die(mysql_error());
+
+  // Good practice to open and close database
+  // mysql_connect("mysql.myhost.com", "user", "sesame") or die(mysql_error());
+  // ... do query stuff ...
+  // mysql_close();
 ?>
