27e29ccd2dfbfcc7477bd265838d003d951d9f8d
Huong Dang <huongd97@gmail.com>
2018-01-25 00:50:17 -0500

    Started on search will finish tomorrow (today)

--- API/searchContact.php
+++ API/searchContact.php
@@ -1,8 +1,16 @@
 <?php
   include "../header.php";
 
-  if (isset($_GET['id']))
-    echo "success";
-  else
+  if !((isset($_GET['id'])))
     echo "fail whale :(";
+
+  if ($conn->connect_error)
+  {
+    echo "fail whale :(";
+  }
+  else
+  {
+    $search = mysqli_real_escape_string($conn, $_GET);
+    
+  }
 ?>
