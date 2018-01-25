62a2f19828e433d2c0e381114b6eaf0c420928fd
Huong Dang <huongd97@gmail.com>
2018-01-25 11:49:35 -0500

    searchContact is done!
    (pending changes after testing)

--- API/searchContact.php
+++ API/searchContact.php
@@ -7,10 +7,45 @@
   if ($conn->connect_error)
   {
     echo "fail whale :(";
+    exit();
   }
   else
   {
-    $search = mysqli_real_escape_string($conn, $_GET);
-    
+    $search = mysqli_real_escape_string($conn, $_GET['id']);
+    $sql = "SELECT * FROM contact WHERE ";
+    $sql = $sql."('FName' LIKE '%".$search."%') OR ";
+    $sql = $sql."('LName' LIKE '%".$search."%') OR ";
+    $sql = $sql."('CellPh' LIKE '%".$search."%') OR ";
+    $sql = $sql."('Email' LIKE '%".$search."%')";
+
+
+    $result = mysqli_query($conn, $sql);
+
+
+    if (mysqli_num_rows($result) > 0)
+    {
+      // Store results in temporary array to be parsed later
+      // into a JSON formatted string
+      $arrResults = array();
+
+      while ($row = mysqli_fetch_assoc($result))
+        array_push($arrayResults, $row['cID']);
+
+
+      $numResults = sizeof($arrayResults);
+
+      $retString = '{"arrResults": [';
+      for ($i = 0; $i < $numResults - 1; $i++)
+        $retString = $retString.'"'.$arrayResults[$i].'", ';
+
+      $retString = $retString.'"'.$arrayREsults[$numResults - 1].'"}';
+
+      echo $retString;
+    }
+    else
+      echo "fail whale :(";
+
+
+    mysqli_close($conn);
   }
 ?>
